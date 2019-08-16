842c0a64ccd860e83afde52b73f809f3c0090303
Rinalds Domanovs <rinalds.domanovs@gmail.com>
2019-08-16 13:03:59 +0300

    Created IncidentUpdateDetailsCell class and linked to storyboard

---
+++ GitHub Status/IncidentUpdatedDetailsCell.swift
@@ -0,0 +1,13 @@
+//
+//  IncidentUpdatedDetailsCell.swift
+//  GitHub Status
+//
+//  Created by Rinalds Domanovs on 16/08/2019.
+//  Copyright © 2019 Rinalds Domanovs. All rights reserved.
+//
+
+import UIKit
+
+class IncidentUpdatedDetailsCell: UITableViewCell {
+
+}
