b1676eb814a445204bf0700052c1545ae338299e
Rinalds Domanovs <rinalds.domanovs@gmail.com>
2019-08-16 15:05:26 +0300

    Setting incident detail cell background color

--- GitHub Status/IncidentUpdatesTableViewController.swift
+++ GitHub Status/IncidentUpdatesTableViewController.swift
@@ -24,13 +24,23 @@ class IncidentUpdatesTableViewController: UITableViewController {
     }
 
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let incident = incidentUpdates[indexPath.row]
         let cell = tableView.dequeueReusableCell(withIdentifier: "IncidentUpdateCell", for: indexPath) as! IncidentUpdatedDetailsCell
+        updateCellUI(cell, indexPath: indexPath)
+        return cell
+    }
+
+    func updateCellUI(_ cell: IncidentUpdatedDetailsCell, indexPath: IndexPath) {
+        let incident = incidentUpdates[indexPath.row]
         cell.idLabel.text = incident.id
         cell.incidentIdLabel.text = incident.incidentId
         cell.statusLabel.text = incident.status
         cell.bodyLabel.text = incident.body
         cell.updatedAtLabel.text = incident.updatedAt
-        return cell
+
+        if incident.status == "resolved" {
+            cell.backgroundColor = UIColor(red: 39 / 255, green: 174 / 255, blue: 96 / 255, alpha: 1)
+        } else {
+            cell.backgroundColor = UIColor(red: 231 / 255, green: 76 / 255, blue: 60 / 255, alpha: 1)
+        }
     }
 }
