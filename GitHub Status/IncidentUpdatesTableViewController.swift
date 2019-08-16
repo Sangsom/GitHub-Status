7cf88257f704ffe9141a9c014b236d0e5492257f
Rinalds Domanovs <rinalds.domanovs@gmail.com>
2019-08-16 11:27:36 +0300

    Created IncidentUpdates class

---
+++ GitHub Status/IncidentUpdatesTableViewController.swift
@@ -0,0 +1,17 @@
+//
+//  IncidentUpdatesTableViewController.swift
+//  GitHub Status
+//
+//  Created by Rinalds Domanovs on 16/08/2019.
+//  Copyright © 2019 Rinalds Domanovs. All rights reserved.
+//
+
+import UIKit
+
+class IncidentUpdatesTableViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+}
