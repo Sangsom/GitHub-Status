afc299720c424aff5be3f5cd263141f7b3804085
Rinalds Domanovs <rinalds.domanovs@gmail.com>
2019-08-16 12:32:22 +0300

    Showing incident updates body in table view

--- GitHub Status/IncidentUpdatesTableViewController.swift
+++ GitHub Status/IncidentUpdatesTableViewController.swift
@@ -16,4 +16,14 @@ class IncidentUpdatesTableViewController: UITableViewController {
         super.viewDidLoad()
     }
 
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return incidentUpdates.count
+    }
+
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "IncidentUpdateCell", for: indexPath)
+        cell.textLabel?.text = incidentUpdates[indexPath.row].body
+        return cell
+    }
+
 }
